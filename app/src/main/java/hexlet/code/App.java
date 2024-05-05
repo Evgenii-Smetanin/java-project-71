ab8529783cfb93915008a506fae7b2075af2b235
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2024-05-05 18:05:55 +0300

    add args & option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(
     name = "gendiff",
@@ -10,6 +12,15 @@ import picocli.CommandLine.Command;
     description = "Compares two configuration files and shows a difference."
 )
 public class App implements Runnable {
+    @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file")
+    private String path1;
+
+    @Parameters(paramLabel = "filepath1", index = "0", description = "path to second file")
+    private String path2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
+
     @Override
     public void run() {
         System.out.println("Hello World!");
