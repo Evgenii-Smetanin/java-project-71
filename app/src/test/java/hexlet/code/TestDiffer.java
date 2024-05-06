f8768609a2b5bc8d3e514da167a0c5a6d8852bf0
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2024-05-06 15:48:05 +0300

    fix accidental changes in tests

--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -16,7 +16,7 @@ public class TestDiffer {
                 + "  - proxy: 123.234.53.22\n"
                 + "  - timeout: 50\n"
                 + "  + timeout: 20\n"
-                + "  +app file1.json file2.json verbose: true\n"
+                + "  + verbose: true\n"
                 + "}";
 
         assertEquals(generate("src/test/resources/file1.json", "src/test/resources/file2.json"), correctResult);
