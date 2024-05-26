8b1d0e0144cc59bd821df86712079b464664d429
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2024-05-26 14:55:43 +0300

    add yaml test

--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -8,17 +8,22 @@ import static hexlet.code.Differ.generate;
 import static org.junit.Assert.assertEquals;
 
 public class TestDiffer {
-    @Test
-    public void testGenerate() throws IOException {
-        String correctResult = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
+    private final String correctResult = "{\n"
+            + "  - follow: false\n"
+            + "    host: hexlet.io\n"
+            + "  - proxy: 123.234.53.22\n"
+            + "  - timeout: 50\n"
+            + "  + timeout: 20\n"
+            + "  + verbose: true\n"
+            + "}";
 
+    @Test
+    public void testGenerateJson() throws IOException {
         assertEquals(generate("src/test/resources/file1.json", "src/test/resources/file2.json"), correctResult);
+    }
+
+    @Test
+    public void testGenerateYaml() throws IOException {
+        assertEquals(generate("src/test/resources/file1.yaml", "src/test/resources/file2.yaml"), correctResult);
     }
 }
