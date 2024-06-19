17c9b15898ed049fe10b855b923cad5ff12a4c9d
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2024-06-19 23:05:53 +0300

    refactor JsonFormatter

--- app/src/main/java/hexlet/code/formatter/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatter/JsonFormatter.java
@@ -5,55 +5,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.Difference;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
 public final class JsonFormatter implements Formatter {
     @Override
     public String format(Set<Difference> difference) {
-        List<Map<String, Object>> differences = new ArrayList<>();
-
-        difference.forEach(d -> {
-            Map<String, Object> diff = new TreeMap();
-            diff.put("key", d.getKey());
-            String operation = "";
-
-            switch (d.getOperation()) {
-                case "+":
-                    operation = "add";
-                    break;
-                case "-":
-                    operation = "delete";
-                    break;
-                case "-+":
-                    operation = "modify";
-                    break;
-                case " ":
-                    operation = "unchanged";
-                    break;
-                default:
-                    throw new RuntimeException("Unknown operation: " + d.getOperation());
-            }
-
-            diff.put("operation", operation);
-            diff.put("value1", d.getLeftVal());
-
-            if (operation.equals("modify")) {
-                diff.put("value2", d.getRightVal());
-            }
-
-            differences.add(diff);
-        });
-
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            return new ObjectMapper().writeValueAsString(differences);
+            return mapper.writeValueAsString(difference);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-
-        return null;
     }
 }
